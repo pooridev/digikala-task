e666f4c0a020c1fd2d26861da3764bfc9c90e35d
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-01-21 14:12:02 +0330

    [fix] not make link active in chat page

--- src/components/Layout/SidebarItem.tsx
+++ src/components/Layout/SidebarItem.tsx
@@ -1,6 +1,6 @@
 // External dependencies
 import { FC } from 'react';
-import { NavLink, RouteMatch } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 // Internal dependencies
 import { SideBarItemProp } from '../../types/components/Layout/sidebar';
@@ -10,9 +10,18 @@ import { SideBarItemProp } from '../../types/components/Layout/sidebar';
  * @returns a `<li />` and an `<a />` element which are used for navigating the user
  */
 const SidebarItem: FC<SideBarItemProp> = ({ path, title, icon }) => {
+  const { pathname } = useLocation();
+
+  // TODO: Bad pattern (better to say, DOG pattern)
+  const isActive =
+    path !== '/' || pathname.includes('contacts')
+      ? pathname.substring(1).replaceAll('/', ' ').includes(path)
+      : pathname.substring(1).includes(path);
+  // debugger;
+
   return (
     <li className='sidebar-item'>
-      <NavLink to={path}>
+      <NavLink className={isActive ? 'active' : ''} to={path}>
         {icon} {title}
       </NavLink>
     </li>
