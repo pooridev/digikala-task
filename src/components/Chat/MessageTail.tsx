712052bdaca7fbead53716a7b8c67583731e9756
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-01-21 18:53:08 +0330

    [fix] linter error due to html attribute naming

--- src/components/Chat/MessageTail.tsx
+++ src/components/Chat/MessageTail.tsx
@@ -1,7 +1,7 @@
 const MessageTail = () => (
   <svg viewBox='0 0 11 20' width='11' height='20' className='message-tail'>
     <svg id='message-tail-filled' viewBox='0 0 11 20'>
-      <g transform='translate(9 -14)' fill='inherit' fill-rule='evenodd'>
+      <g transform='translate(9 -14)' fill='inherit' fillRule='evenodd'>
         <path
           d='M-6 16h6v17c-.193-2.84-.876-5.767-2.05-8.782-.904-2.325-2.446-4.485-4.625-6.48A1 1 0 01-6 16z'
           transform='matrix(1 0 0 -1 0 49)'
